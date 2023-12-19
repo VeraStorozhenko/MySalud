2f170c75681c218169a956d55fb6f4aa843085c6
vera <verun.mak@gmail.com>
2023-12-19 15:33:51 +0100

    basic functionality

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,58 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  def create
+    build_resource(sign_up_params)
+
+    role = params[:user][:role]
+    if role == 'patient'
+      resource.build_patient(patient_params)
+    elsif role == 'doctor'
+      resource.build_doctor(doctor_params)
+    end
+
+    if resource.save
+      sign_in(resource)
+      p '=after sign in='
+      p current_user
+      if role == 'patient'
+        redirect_to patients_appointments_path, notice: "User was successfully created."
+      elsif role == 'doctor'
+        redirect_to appointments_doctors_path, notice: "User was successfully created."
+      end
+    else
+      render :template => 'users/registrations/new', locals: { resource: resource}
+    end
+  end
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :role])
+  end
+
+  def doctor_params
+    params.require(:user).require(:doctor).permit(
+      :city,
+      :specialty,
+      :clinic_name,
+      :description,
+    )
+  end
+
+  def patient_params
+    params.require(:user).require(:patient).permit(
+      :date_of_birth,
+      )
+  end
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    if resource.patient?
+      patients_appointments_path
+    elsif resource.doctor?
+      appointments_doctors_path
+    end
+  end
+end
